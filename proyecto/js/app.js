53ff90a100cc2fe6ae529151324b3b72b0ee1639
cmagnata <c.magnataveragua@uandresbello.edu>
2026-09-02 22:44:44 -0400

    Implementation of form and user view

---
+++ proyecto/js/app.js
@@ -0,0 +1,26 @@
+
+Fila_turnos[{
+    id,
+    tipo_turno,
+    valor,
+}]
+
+
+
+
+
+
+//Esto debe ir dentro de la función que maneja la lógica de la creación de los numeros
+function crearTurno(tipo_turno) {
+    if (tipo_turno === 'General') {
+
+    }
+
+    else if (tipo_turno === 'Toma de muestra') {
+
+    }
+
+    else if (tipo_turno === 'Pagos') {
+
+    }
+}
